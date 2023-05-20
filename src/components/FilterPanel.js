6ad13608903ab3bce70d149ed0b65bb1c78dd8ad
Thanatas42 <dmitrodonskoy@yandex.ru>
2023-05-20 14:49:19 +0300

    Поправил поиск

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -1,50 +1,72 @@
 import React from "react";
 import styles from '@/styles/Home.module.scss'
 
-
 export default function FilterPanel(props) {
     const [filterActive, SetFilterActive] = React.useState(false);
     const [filter, SetFilter] = React.useState(new Set());
     const [showFilterResult, SetShowFilterResult] = React.useState(0);
 
-    function handleChangeSubmit(e) {
+    async function getDataApi(carBrandsArr, count = false) {
+        let results = [];
+        let length = 0;
+
+        for (let i = 0; i < carBrandsArr.length; i++) {
+            await fetch(`${props.config.api}?brand=${carBrandsArr[i]}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    if (count) {
+                        length += data.list.length;
+                    } else {
+                        data.list.forEach((item) => {
+                            results.push(item);
+                        });
+                    }
+                });
+        }
+
+        return count ? length : results;
+    }
+
+    async function handleChangeSubmit(e) {
         e.preventDefault();
 
         let array = filter.size > 0 ? Array.from(filter) : props.config.auto;
-        let results = [];
-        for (let i = 0; i < array.length; i++) {
-            fetch(`${props.config.api}?brand=${array[i]}`)
-                .then((res) => res.json())
-                .then((data) => {
-                    results = results.concat(data.list);
-                    props.SetCarsList(results);
-                });
-        }
+
+        props.SetCarsList(await getDataApi(array));
     }
 
-    function handleChangeFilterPanel(e) {
+    function handleChangeVisibleFilterPanel(e) {
         SetFilterActive(!filterActive);
-        document.getElementsByName('filterInput').forEach(element => { element.classList.add(styles.hidden) });
+
+        document.getElementsByName('filterInput')
+            .forEach(element => { element.classList.add(styles.hidden) });
     }
 
-    function handleChangeFilter(e) {
+    async function handleChangeFilter(e) {
+        let checkArray = [];
+        SetShowFilterResult('...');
+
+        document.getElementsByName('filterInputCheckbox').forEach(c => {
+            if (e.target.checked) {
+                c.checked ? document.getElementById(`button${c.id}`).classList.add(styles.hidden) : '';
+            } else {
+                c.checked ? checkArray.push(c) : '';
+            }
+        });
+
         if (e.target.checked) {
-            document.getElementsByName('filterInput').forEach(element => { element.classList.add(styles.hidden) });
-            document.getElementById(`button${e.target.id}`).classList.remove(styles.hidden);
-        }
+            document.getElementById(`button${e.target.id}`).classList.remove(styles.hidden)
+        } else {
+            checkArray.length > 0 ? document.getElementById(`button${checkArray.at(-1).id}`).classList.remove(styles.hidden) : '';
+            document.getElementById(`button${e.target.id}`).classList.add(styles.hidden)
+        };
 
         filter.has(e.target.value) ?
             filter.delete(e.target.value) : filter.add(e.target.value);
         SetFilter(filter);
 
         let array = Array.from(filter);
-        let count = 0;
-
-        array.forEach((auto) => {
-            count += props.cars.filter((item) => item.feedData.brandName === auto).length;
-        })
-        if (array.length === 0)
-            count = props.cars.length;
+        let count = array.length === 0 ? props.cars.length : await getDataApi(array, true);
 
         SetShowFilterResult(count);
     }
@@ -52,16 +74,16 @@ export default function FilterPanel(props) {
 
     return (
         <div className={styles.filterPanel}>
-            <div onClick={handleChangeFilterPanel}>
+            <div onClick={handleChangeVisibleFilterPanel}>
                 <p>Марка</p><span className={filterActive ? styles.active : ''} />
             </div>
-            <form className={filterActive ? '' : styles.hidden} onChange={handleChangeFilter} onSubmit={handleChangeSubmit}><ul>
+            <form id={'filterForm'} className={filterActive ? '' : styles.hidden} onChange={handleChangeFilter} onSubmit={handleChangeSubmit}><ul>
                 {props.config.auto.map((item, index) => {
                     return <li key={index}>
-                        <input type="checkbox" id={index} name={item} value={item} />
+                        <input type="checkbox" id={index} name={'filterInputCheckbox'} value={item} />
                         <label htmlFor={index}>{item}</label>
                         <input className={styles.hidden} name={'filterInput'} id={`button${index}`} type="submit"
-                            value={showFilterResult > 0 ? `Показать ${showFilterResult} автомобилей` : "Автомобили не найдены"} />
+                            value={showFilterResult === 0 ? "Автомобили не найдены" : `Показать ${showFilterResult} автомобилей`} />
                     </li>
                 })}
             </ul></form>
